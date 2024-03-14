f70bdc50d78afc4ab97e81239b3d3c93515c4b9f
wangchunqing <2335705645@qq.com>
2024-03-14 16:59:28 +0800

    普通页和开发页

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -2,7 +2,17 @@
   <view class="content">
     <image class="logo" src="/static/logo.png" />
     <view class="text-area">
-      <text class="title">{{ memberStore.title }}</text>
+      <view class="title" style="align-self: center">{{
+        memberStore.title
+      }}</view>
+      <text class="title">区分开发页和普通页 </text>
+      <text class="title"
+        >开发页为在tabBar中注册的页面，跳转该页无法返回上一页
+      </text>
+      <text class="title">普通页只在pages中注册，可以返回上一页 </text>
+      <text class="title"
+        >有时候需要跳转到开发页，但是能再返回上一级，此时只需要将功能代码封装到一个页面，在开发页和普通页中引用他，这样就免去了复制两份代码</text
+      >
     </view>
   </view>
 </template>
@@ -32,7 +42,7 @@ const memberStore = useMemberStore();
 
 .text-area {
   display: flex;
-  justify-content: center;
+  flex-direction: column;
 }
 
 .title {
